9af5c9f2e9fbb4d0ba3ce74c7bf5ae281f39f99b
Jacky Liang <bluelegopiece@hotmail.com>
2015-03-02 20:41:21 +0800

    Added a non-functional colorize() function to color the website type

--- coven-cli.php
+++ coven-cli.php
@@ -1,10 +1,26 @@
 <?php
 
-const CACHE = "/.coven_cache";
+const CACHE = "/.coven-cache";
 const COVEN_API = "http://api.coven.link/api/v1/posts"; 
+const NUM_POSTS = 20;
+
+// Colors!!
+// I was going to match coven.link's way of representing the website
+// type using a background color + white text, but CLI outputs only
+// support so few colors =(. Maybe I am wrong, though.
+const WHITE = '[1;37m';
+const LIGHT_BLUE = '[1;34m';
+const YELLOW = '[1;33m';
+const RED = '[0;31m';
+const BROWN = '[0;33m';
+const COLOR_BREAK = '\033[0m';
 
 /**
- * Coven CLI 
+ * Coven 
+ * The only news you need
+ * This is my first project done entirely in vi, so please bare with
+ * me on the illogically place code. I am trying my best to use this
+ * project to learn it!
  */
  
 echo '---------------------------------------' . PHP_EOL;
@@ -30,8 +46,40 @@ if(!file_exists(dirname(__FILE__) . CACHE)) {
 $jsonObject = json_decode($jsonRaw);
 
 foreach($jsonObject as $key => $value) {
-    echo '[ ' . $value->source_data->symbol . ' ] ' .  $value->position . '. ' . $value->title . PHP_EOL;
-    if($key === 20) {
+    echo '[ ' . $value->source_data->symbol . ' ] ' .  
+    $value->position . '. ' . 
+    $value->title . 
+    PHP_EOL;
+    // TODO: think of a more elegant way to only show twenty results
+    if($key === NUM_POSTS) {
         break;
     }
 }
+/**
+ * Map website-type to color-formatted type  
+ * TODO: Ok.. It doesn't seem like this works on my Mac Terminal.
+ * Investigate how I can get purdy colors on here too
+ * @param char    $symbol        A website-type char 
+ * @return string $coloredSymbol Colored website-type
+ */
+function colorize($symbol) {
+    $coloredSymbol = ''; 
+    switch($symbol) {
+        case 'r':
+            $coloredSymbol = LIGHT_BLUE . ' r ' . COLOR_BREAK;
+            break;
+        case 'L':
+            $coloredSymbol = BROWN . ' L ' . COLOR_BREAK;
+            break;
+        case 'Y':
+            $coloredSymbol = YELLOW . ' Y ' . COLOR_BREAK;
+            break;
+        case 'P':
+            $coloredSymbol = RED . ' P ' . COLOR_BREAK;
+            break;
+        default:
+            $coloredSymbol = '';
+    }
+    return $coloredSymbol;
+}
+
