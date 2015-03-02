7b3321e92e4f795a5cd8c50cbcac132353e53258
Jacky Liang <bluelegopiece@hotmail.com>
2015-03-02 17:41:58 +0800

    Added caching to getting JSON data

---
+++ coven-cli.php
@@ -0,0 +1,33 @@
+<?php
+
+const CACHE = "/.coven_cache";
+const COVEN_API = "http://api.coven.link/api/v1/posts"; 
+
+/**
+ * Coven CLI 
+ */
+ 
+echo '---------------------------------------' . PHP_EOL;
+echo '     Coven - The only news you need    ' . PHP_EOL;
+echo '---------------------------------------' . PHP_EOL;
+
+// Check cached JSON
+if(!file_exists(dirname(__FILE__) . CACHE)) {
+    // Get the file from api.coven.link if there's no cached version 
+    $jsonRaw = file_get_contents(COVEN_API);
+    file_put_contents(
+        dirname(__FILE__) . CACHE, 
+        $jsonRaw
+    );
+} else {
+    // Otherwise, use the cached version
+    $jsonRaw = file_get_contents(
+        dirname(__FILE__) . CACHE
+    );
+}
+
+// Convert string to json object
+$jsonObject = json_decode($jsonRaw);
+
+var_dump($jsonObject);
+
